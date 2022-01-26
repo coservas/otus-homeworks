6686895d639159167c854ef7dde2a767df431941
Васильев Сергей Романович <sergejj.vasilev@vseinstrumenti.ru>
2022-01-26 04:26:44 +0300

    Реализация основной части задачи

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,46 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var result strings.Builder
+	var prev rune
+
+	for i, cur := range input {
+		isDigit := unicode.IsDigit(cur)
+		if i == 0 && isDigit {
+			return "", ErrInvalidString
+		}
+
+		isDigitPrev := unicode.IsDigit(prev)
+		if isDigitPrev && isDigit {
+			return "", ErrInvalidString
+		}
+
+		if !isDigit {
+			result.WriteRune(cur)
+			prev = cur
+			continue
+		}
+
+		if cur == '0' {
+			tempString := strings.TrimSuffix(result.String(), string(prev))
+			result.Reset()
+			result.WriteString(tempString)
+			continue
+		}
+
+		for j := rune(1); j < cur-'0'; j++ {
+			result.WriteRune(prev)
+		}
+
+		prev = cur
+	}
+
+	return result.String(), nil
 }
