2a7100a61e169fa7ea08ce4e1713a924de3852b7
Васильев Сергей Романович <sergejj.vasilev@vseinstrumenti.ru>
2022-02-22 23:48:14 +0300

    5 - Решение основной задачи

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	doneStage := func(in In) Out {
+		out := make(Bi)
+		go func() {
+			defer close(out)
+			for {
+				select {
+				case v, ok := <-in:
+					if !ok {
+						return
+					}
+					out <- v
+				case <-done:
+					return
+				}
+			}
+		}()
+
+		return out
+	}
+
+	out := doneStage(in)
+
+	if len(stages) == 1 && stages[0] == nil {
+		return out
+	}
+
+	for _, stage := range stages {
+		out = doneStage(stage(out))
+	}
+
+	return out
 }
