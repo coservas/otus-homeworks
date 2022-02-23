20a5d39a0168e15df2f50fd422512a7e44deba92
Васильев Сергей Романович <sergejj.vasilev@vseinstrumenti.ru>
2022-02-24 00:40:42 +0300

    7 - Добавление тестов

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,22 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	fromPath := "testdata/input.txt"
+	toPath := "/tmp/output.txt"
+
+	t.Run("unsupported file", func(t *testing.T) {
+		err := Copy("/dev/urandom", toPath, 0, 0)
+		require.EqualError(t, err, ErrUnsupportedFile.Error())
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		err := Copy(fromPath, toPath, 9999, 0)
+		require.EqualError(t, err, ErrOffsetExceedsFileSize.Error())
+	})
 }
