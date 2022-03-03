9f40f3e5260b3296504b32032ee35cb4ce0ad480
Васильев Сергей Романович <sergejj.vasilev@vseinstrumenti.ru>
2022-03-04 02:55:38 +0300

    8 - Добавление тестов для executor.go

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,41 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("-1", func(t *testing.T) {
+		var env Environment
+		cmd := []string{
+			"not_found_cmd",
+			"-somekey",
+		}
+
+		code := RunCmd(cmd, env)
+		require.Equal(t, -1, code)
+	})
+
+	t.Run("0", func(t *testing.T) {
+		var env Environment
+		cmd := []string{
+			"ls",
+			"-l",
+		}
+
+		code := RunCmd(cmd, env)
+		require.Equal(t, 0, code)
+	})
+
+	t.Run("1", func(t *testing.T) {
+		var env Environment
+		cmd := []string{
+			"git",
+		}
+
+		code := RunCmd(cmd, env)
+		require.Equal(t, 1, code)
+	})
 }
